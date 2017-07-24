eab80a0e6e428566c24b14ee45672d4b947d9289
Matiezis <matiezisu@gmail.com>
2017-07-24 10:32:54 +0200

    Basic functionality.
    Supports raw text input and text file input. Counts numer of words and
    number of unique words used in text.

---
+++ WordCounter/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace WordCounter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.listViewUniqueWords = new System.Windows.Forms.ListView();
+            this.columnHeaderWord = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderCount = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxUniqueWordCount = new System.Windows.Forms.TextBox();
+            this.textBoxWordCount = new System.Windows.Forms.TextBox();
+            this.buttonBrowse = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 36);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(252, 367);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // listViewUniqueWords
+            // 
+            this.listViewUniqueWords.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeaderWord,
+            this.columnHeaderCount});
+            this.listViewUniqueWords.Location = new System.Drawing.Point(311, 36);
+            this.listViewUniqueWords.Name = "listViewUniqueWords";
+            this.listViewUniqueWords.Size = new System.Drawing.Size(372, 367);
+            this.listViewUniqueWords.TabIndex = 1;
+            this.listViewUniqueWords.UseCompatibleStateImageBehavior = false;
+            this.listViewUniqueWords.View = System.Windows.Forms.View.Details;
+            this.listViewUniqueWords.ColumnClick += new System.Windows.Forms.ColumnClickEventHandler(this.listViewUniqueWords_ColumnClick);
+            // 
+            // columnHeaderWord
+            // 
+            this.columnHeaderWord.Text = "Word";
+            this.columnHeaderWord.Width = 173;
+            // 
+            // columnHeaderCount
+            // 
+            this.columnHeaderCount.Text = "Count";
+            this.columnHeaderCount.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.columnHeaderCount.Width = 132;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(471, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Output";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(110, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Input";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(689, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Unique Word Count";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(726, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Word Count";
+            // 
+            // textBoxUniqueWordCount
+            // 
+            this.textBoxUniqueWordCount.Location = new System.Drawing.Point(796, 36);
+            this.textBoxUniqueWordCount.Name = "textBoxUniqueWordCount";
+            this.textBoxUniqueWordCount.Size = new System.Drawing.Size(100, 20);
+            this.textBoxUniqueWordCount.TabIndex = 6;
+            // 
+            // textBoxWordCount
+            // 
+            this.textBoxWordCount.Location = new System.Drawing.Point(796, 72);
+            this.textBoxWordCount.Name = "textBoxWordCount";
+            this.textBoxWordCount.Size = new System.Drawing.Size(100, 20);
+            this.textBoxWordCount.TabIndex = 7;
+            // 
+            // buttonBrowse
+            // 
+            this.buttonBrowse.Location = new System.Drawing.Point(608, 459);
+            this.buttonBrowse.Name = "buttonBrowse";
+            this.buttonBrowse.Size = new System.Drawing.Size(75, 23);
+            this.buttonBrowse.TabIndex = 8;
+            this.buttonBrowse.Text = "Browse";
+            this.buttonBrowse.UseVisualStyleBackColor = true;
+            this.buttonBrowse.Click += new System.EventHandler(this.buttonBrowse_Click);
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label5.Location = new System.Drawing.Point(502, 459);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 23);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Please Select File...";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(994, 574);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.buttonBrowse);
+            this.Controls.Add(this.textBoxWordCount);
+            this.Controls.Add(this.textBoxUniqueWordCount);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listViewUniqueWords);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Word Counter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ListView listViewUniqueWords;
+        private System.Windows.Forms.ColumnHeader columnHeaderWord;
+        private System.Windows.Forms.ColumnHeader columnHeaderCount;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxUniqueWordCount;
+        private System.Windows.Forms.TextBox textBoxWordCount;
+        private System.Windows.Forms.Button buttonBrowse;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ WordCounter/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WordCounter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            listViewUniqueWords.Items.Clear();
+            List<string> uniqueWordsList = new List<string>();
+            List<int> uniqueWordsCountList = new List<int>();
+            string wholeText = richTextBox1.Text;
+            wholeText = wholeText.ToLower();
+            string[] separators = { "\n","\r\n","/","\\","|","-","+","(",")","<",">","[","]","$","#","!"," ", ",", ".", "?", "!", "    ", "...", "'", ";", ":","1","2","3","4","5","6","7","8","9","0","*","-","_","^","@","#" };
+            string[] words = wholeText.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            foreach(string word in words)
+            {
+                if (uniqueWordsList.Contains(word))
+                {
+                    int index = uniqueWordsList.IndexOf(word);
+                    uniqueWordsCountList[index]++;
+                }
+                else
+                {
+                    uniqueWordsList.Add(word);
+                    uniqueWordsCountList.Add(1);
+                }
+            }
+            if (uniqueWordsList.Count > 0)
+            {
+                int uniqueWordCount = 0;
+                int wordCount = 0;
+                foreach (string word in uniqueWordsList)
+                {
+                    uniqueWordCount++;
+                    wordCount += uniqueWordsCountList[uniqueWordsList.IndexOf(word)];
+                    ListViewItem tempItem = new ListViewItem(word);
+                    tempItem.SubItems.Add(uniqueWordsCountList[uniqueWordsList.IndexOf(word)].ToString());
+                    listViewUniqueWords.Items.Add(tempItem);
+                }
+                textBoxUniqueWordCount.Text = uniqueWordCount.ToString();
+                textBoxWordCount.Text = wordCount.ToString(); 
+            }
+
+        }
+        private int sortColumn = -1;
+        public class ListViewItemComparer : IComparer
+        {
+            private int col;
+            private System.Windows.Forms.SortOrder order;
+            public ListViewItemComparer()
+            {
+                col = 0;
+                order = System.Windows.Forms.SortOrder.Ascending;
+            }
+            public ListViewItemComparer(int column, System.Windows.Forms.SortOrder order)
+            {
+                col = column;
+                this.order = order;
+            }
+            public int Compare(object x, object y)
+            {
+                int returnVal = -1;
+                returnVal = String.Compare(((ListViewItem)x).SubItems[col].Text,
+                                ((ListViewItem)y).SubItems[col].Text);
+                if (col == 1)
+                {
+                    returnVal = String.Compare(((ListViewItem)x).SubItems[col].Text,
+                                ((ListViewItem)y).SubItems[col].Text);
+                }
+                if (order == System.Windows.Forms.SortOrder.Descending)
+                    returnVal *= -1;
+                return returnVal;
+            }
+        }
+        private void listViewUniqueWords_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
+            if (e.Column != sortColumn)
+            {
+                sortColumn = e.Column;
+                listViewUniqueWords.Sorting = SortOrder.Ascending;
+            }
+            else
+            {
+                if (listViewUniqueWords.Sorting == SortOrder.Ascending)
+                    listViewUniqueWords.Sorting = SortOrder.Descending;
+                else
+                    listViewUniqueWords.Sorting = SortOrder.Ascending;
+            }
+
+            listViewUniqueWords.Sort();
+            this.listViewUniqueWords.ListViewItemSorter = new ListViewItemComparer(e.Column, listViewUniqueWords.Sorting);
+        }
+
+        private void buttonBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Text files|*.txt";
+            openFileDialog1.Title = "Select a Text File";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.Text = File.ReadAllText(openFileDialog1.FileName);                
+            }
+        }
+    }
+}
